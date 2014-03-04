ad187caadff232c9d2fa0209ebac7008feac657c
Bobie <bobie@herxun.co>
2014-03-04 14:24:00 +0800

    Initial Commit

---
+++ HandyFlickr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HandyFlickr
+//
+//  Created by Bobie Chen on 2014/3/4.
+//  Copyright (c) 2014年 Bobie Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
